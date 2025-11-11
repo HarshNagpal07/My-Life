85bd93ec729994f828925efacefe6c6cf7732914
Harsh Nagpal <hnagpal056@gmail.com>
2025-11-11 18:45:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { use, useEffect, useState } from 'react';
+import { getPosts } from './api'; 
 import './App.css';
 
 function App() {
+
+  const [data,setData]=useState(null);
+  useEffect(()=>{
+    getPosts().then(posts => setData(posts));
+  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
